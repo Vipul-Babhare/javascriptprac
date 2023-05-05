8a9d74937c871365a23716832ed0f7f137f4db2f
Vipul-Babhare <121046919+Vipul-Babhare@users.noreply.github.com>
2023-05-05 11:55:00 +0530

    Add files via upload

---
+++ StringsFunt.js
@@ -0,0 +1,6 @@
+let name = "Vipul\""
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.slice(2,4))//4th character not included
+console.log(name.slice(2));
+console.log(name.replace("Vi","Pi"))
