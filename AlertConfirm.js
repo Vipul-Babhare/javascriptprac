42f3eb294ecbcf190f36c43214c008606480ed79
Vipul-Babhare <121046919+Vipul-Babhare@users.noreply.github.com>
2023-05-06 12:44:16 +0530

    Add files via upload

---
+++ AlertConfirm.js
@@ -0,0 +1,9 @@
+alert("Enter the value of a ")
+let a = prompt("Enter the value of a")
+a = Number.parseInt(a)
+alert("You entered a of type")
+ler write = confirm("Do u want write it to the page")
+if(write)
+{
+  document.write(a)
+}
